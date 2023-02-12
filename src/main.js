634e97870b045b5e43222c94a46289acdf6dfd76
Newbiesaurus <newbiesaurus@gmail.com>
2023-02-12 09:57:56 -0700

    Cosmetic

--- src/main.js
+++ src/main.js
@@ -8,9 +8,7 @@ export default {
         <h2 class="font-serif font-bold text-2xl
             text-red-600 bg-sky-600"><div class ="justify-center">
             <label class="bg-blue-400 rounded p-2 border-8 border-sky-600 text-center">
-            Infusi
-            <img class="h-8 w-8 bottom-100" src ="/src/assets/Timer.png" />
-            n Timer</label></div>
+            Infusi<label class="text-blue-600">o</label>n Timer</label></div>
         </h2>
         <infusions></infusions>
     `,
