9786428591c4a4f11a9a9a7517e088e7f79e237d
Newbiesaurus <99927416+Newbiesaurus@users.noreply.github.com>
2023-02-14 08:55:23 -0700

    Aesthetics

--- src/components/InfusionTab.js
+++ src/components/InfusionTab.js
@@ -1,7 +1,7 @@
 export default {
     template: `
         <button v-if="tabSet.name != title" @click="tabS" class="bg-blue-500 hover:bg-blue-600 rounded p-2">{{ title }} ({{infusions.length}})</button>
-        <button v-else @click="shrinkTab" class="bg-blue-200 rounded p-2 border-t border-blue-500">{{ title }} ({{infusions.length}})</button>
+        <button v-else @click="shrinkTab" class="bg-blue-200 rounded p-2 border-t-4 border-blue-600">{{ title }} ({{infusions.length}})</button>
     `,
 
     methods: {
