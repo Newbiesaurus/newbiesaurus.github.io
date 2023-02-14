c2b9d92d3d27f9fb9c2329689606f2e5f7ba2363
Newbiesaurus <99927416+Newbiesaurus@users.noreply.github.com>
2023-02-14 08:52:51 -0700

    Aesthetics

--- src/components/Infusion.js
+++ src/components/Infusion.js
@@ -2,49 +2,51 @@ import InfusionUnits from "./InfusionUnits.js"
 import InfusionEdit from "./InfusionEdit.js";
 import InfusionTimer from "./InfusionTimer.js";
 
-
 export default {
     components: { InfusionUnits, InfusionEdit, InfusionTimer },
 
-
     template: `
-        <li v-if="!infusion.edit">
-        <div class="border-8 border-blue-200">
-        <div class="border border-blue-300"><label>Infusion:</label> <label class="text-blue-800 text-2xl">{{infusion.name}}
-            <label v-if="infusion.units > 1">{{infusion.concentration}}</label>
-            (<infusion-units :infusion="infusion"></infusion-units>
-            <label v-if="infusion.units > 1">/mL</label>)</label>
-            </div>
+    <li v-if="!infusion.edit">
+	    <div class="border-b-8 border-blue-300">
+        	<div class="border-b-4 border-blue-300">Infusion:  
+			<label class="text-blue-800 text-2xl">{{ infusion.name }} 
+            			<label v-if="infusion.units > 1">{{ infusion.concentration }}</label>
+            			(<infusion-units :infusion="infusion"></infusion-units>
+            			<label v-if="infusion.units > 1">/mL</label>)
+			</label>
+		    </div>
 
+        	<div class="border-b-4 border-blue-300">Rate: 
+			    <label class="text-blue-800 md:text-l text-2xl">{{infusion.rate}} mL/hr</label>
+		    </div>
+            
+        	<div class="border-b-4 border-blue-300" v-show="infusion.units > 1">
+			    <label>Dose: {{ Dose }} <infusion-units :infusion="infusion"></infusion-units> 
+        			<label v-show="infusion.weight > 1">/kg</label>
+				/hr
+			    </label>
+		    </div>
 
-        <div class="border border-blue-300"><label>Rate:</label> <label class="text-blue-800 md:text-l text-2xl">{{infusion.rate}} mL/hr</div>
-        <div class="border border-blue-300" v-show="infusion.units > 1"><label>Dose: {{ Dose }} <infusion-units :infusion="infusion"></infusion-units>
-        <label v-show="infusion.weight > 1">/kg</label>/hr</label> </div>
+        	<div><infusion-timer :infusion="infusion" @save="save"></infusion-timer></div>
 
+        	<div class="border-4 border-blue-200 flex justify-between">
+            		<button class="text-white bg-blue-600 hover:bg-blue-800 
+            		rounded px-7 py-2 p-2" @click="infusion.edit = true">Edit</button>
 
-        <div><infusion-timer :infusion="infusion" @save="save"></infusion-timer><div>
+            		<div>Favorites: <input type="checkbox" @change="save" v-model="infusion.favorites" /></div>
 
-
-        <div class="border border-blue-300 flex justify-between">
-            <button class="text-white bg-blue-600 hover:bg-blue-800
-            rounded px-7 py-2 p-2" @click="infusion.edit = true">Edit</button>
-
-
-            <div>Favorites: <input type="checkbox" @change="save" v-model="infusion.favorites" /></div>
-
-
-            <button @click="deleteDrip" class="text-white bg-blue-600 hover:bg-blue-800
-            rounded px-7 py-2 p-2">Delete</button>
+            		<button @click="deleteDrip" class="text-white bg-blue-600 hover:bg-blue-800 
+            		rounded px-7 py-2 p-2">Delete</button>
+        	</div>
         </div>
-        </div>
-        </li>
-        <li v-else><infusion-edit :infusion="infusion" @save="save"></infusion-edit></li>
+    </li>
+
+    <li v-else><infusion-edit :infusion="infusion" @save="save"></infusion-edit></li>
     `,
-   
+    
     methods: {
         deleteDrip() {
-            this.infusion.active = false;
-
+            this.infusion.active = false; 
 
             delete this.infusion.name;
             delete this.infusion.concentration;
@@ -61,25 +63,20 @@ export default {
             delete this.infusion.active;
             delete this.infusion.id;
 
-
             this.save()
         },
-
 
         save() {
             this.$emit('saveToList')
         }
 
-
     },
-
 
     computed: {
         Dose() {
-            return (this.infusion.rate * this.infusion.concentration)/this.infusion.weight
+            return (this.infusion.rate * this.infusion.concentration)/this.infusion.weight 
         },
     },
-
 
     props: {
         infusion: Object
