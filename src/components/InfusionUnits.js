2dfa760991b496b73cb65d4f6c4b2acbfff9b249
Newbiesaurus <99927416+Newbiesaurus@users.noreply.github.com>
2023-02-14 00:17:34 -0700

    Units change

--- src/components/InfusionUnits.js
+++ src/components/InfusionUnits.js
@@ -3,7 +3,7 @@ export default {
         <label v-if="infusion.units < 2">mL</label>
         <label v-else-if="infusion.units < 3">g</label>
         <label v-else-if="infusion.units < 4">mg</label>
-        <label v-else-if="infusion.units < 5">g</label>
+        <label v-else-if="infusion.units < 5">mcg</label>
         <label v-else>u</label>
     `,
 
