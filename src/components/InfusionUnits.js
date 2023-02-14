d57ee1a44532dd0d06ea688bac942377d51b76e3
Newbiesaurus <99927416+Newbiesaurus@users.noreply.github.com>
2023-02-14 11:16:32 -0700

    Mess

--- src/components/InfusionUnits.js
+++ src/components/InfusionUnits.js
@@ -1,12 +1,12 @@
 export default {
     template: `
         <label v-if="infusion.units < 2">mL</label>
-        <label v-else-if="infusion.units < 3">g</label>
-        <label v-else-if="infusion.units < 4">mg</label>
-        <label v-else-if="infusion.units < 5">mcg</label>
+        <label v-else-if="infusion.units < 3">gtt</label>
+        <label v-else-if="infusion.units < 4">g</label>
+        <label v-else-if="infusion.units < 5">mg</label>
+        <label v-else-if="infusion.units < 6">mcg</label>
         <label v-else>u</label>
     `,
-
 
     props: {
         infusion: Object
